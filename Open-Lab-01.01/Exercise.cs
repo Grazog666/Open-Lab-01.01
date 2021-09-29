b1e235de6ede7d018564bc32545a5993d00dabb0
Ján Kubaška <jajanokubaska@gmail.com>
2021-09-29 10:16:33 +0200

    ...

--- Open-Lab-01.01/Exercise.cs
+++ Open-Lab-01.01/Exercise.cs
@@ -1,12 +1,21 @@
 ﻿using System;
-
 namespace Open_Lab_01._01
 {
     public class Exercise
     {
         public bool IsLessThan10(string input)
         {
-            throw new NotImplementedException();
+            int gg;
+            Int32.TryParse(input, out gg);
+            if (gg < 10)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+                ;
+            }
         }
     }
 }
